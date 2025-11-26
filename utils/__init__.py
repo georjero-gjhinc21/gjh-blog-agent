47d756eb10043d35921acbbe4667f85604e1d28c
George Jero <george@gjhconsulting.net>
2025-11-25 23:16:08 -0600

    Update utils package exports for PartnerStack

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 """Utilities package."""
 from .ollama_client import OllamaClient
 from .vector_store import VectorStore
+from .partnerstack_client import PartnerStackClient
 
-__all__ = ["OllamaClient", "VectorStore"]
+__all__ = ["OllamaClient", "VectorStore", "PartnerStackClient"]
